ace8065d24b77b94f565189309d1dd1b34f92ead
Hữu Vinh <42138301+huuvinhtv@users.noreply.github.com>
2024-08-07 00:51:30 +0700

    Delete src directory

--- src/features/store.js
+++
@@ -1,5 +0,0 @@
-import { configureStore } from "@reduxjs/toolkit";
-import musicPlayerReducer from "./MusicPlayer/musicplayer.slice";
-const store = configureStore({ reducer: { musicplayer: musicPlayerReducer } });
-
-export default store;
